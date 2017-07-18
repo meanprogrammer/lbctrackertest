8064a151383b1f5c400c2db8e9d2a51a690608ce
meanprogrammer <mean.programmer.d@gmail.com>
2017-07-18 16:41:14 +0800

    ter

--- LBCTrackingTest/LBCTrackingTest/index.aspx
+++ LBCTrackingTest/LBCTrackingTest/index.aspx
@@ -10,6 +10,7 @@
     <form id="form1" runat="server">
     <div>
         <h1>Index</h1>
+        <asp:Literal ID="Literal1" runat="server"></asp:Literal>
     </div>
     </form>
 </body>
--- LBCTrackingTest/LBCTrackingTest/index.aspx.cs
+++ LBCTrackingTest/LBCTrackingTest/index.aspx.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using LBCTrackingTest.LBCAPI;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -11,7 +13,27 @@ namespace LBCTrackingTest
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            string[] trackings = new string[] { "311216816471", "321128079188", "311216695131", "311216695121" };
+            LBCAPI.Service service = new LBCAPI.Service();
 
+            StringBuilder b = new StringBuilder();
+
+            foreach (string tracking in trackings)
+            {
+                shipmentFields[] sfs = service.trackandtraceDetails(tracking);
+
+                foreach (var item in sfs)
+                {
+                    //Console.WriteLine(string.Format("Tracking #: {0}", item.TrackingNo));
+                    b.AppendFormat("Tracking #: {0}", item.TrackingNo);
+                    //Console.WriteLine(string.Format("Status: {0}", item.statusMessage));
+                    b.AppendFormat("Status: {0}", item.statusMessage);
+                }
+
+            }
+
+            this.Literal1.Text = b.ToString();
+            //Console.ReadLine();
         }
     }
 }
